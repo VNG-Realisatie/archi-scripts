13e82cea30354abd1fc4d807ba63c46ad25d60d7
Mark Backer <mark.backer@vng.nl>
2021-02-27 17:33:36 +0100

    WIP import relation

--- WIP/export_import_functions.js
+++ WIP/export_import_functions.js
@@ -6,10 +6,26 @@
  * 
  */
 
+// Property with a logical key (your own unique ID's, independent of used architecture tool)
+const PROP_ID = "Object ID";
+
 const ATTRIBUTE_LABELS = ["id", "type", "name", "documentation"];
-const SKIP_ATTRIBUTE_ID = "id"
-const ENDPOINT_LABELS = ["source", "target"];
-const ENDPOINT_ATTRIBUTE_LABELS = ["name", "id"];
+const SKIP_IMPORT_LABELS = ["id", "source.name", "target.name", "source.id", "target.id", "source.prop('Object ID')", "target.prop('Object ID')"]
+
+const ENDPOINTS = ["source", "target"];
+const ENDPOINT_PROP_ID = `prop('${PROP_ID}')`
+const ENDPOINT_LABELS = ["name", "id", ENDPOINT_PROP_ID];
+
+// scanned row actions
+const PROP_ID_SYNC = "PROP_ID sync";
+const ID_SYNC = "id sync";
+const NAME_SYNC = "name sync";
+const ENDPOINT_SYNC = "endpoint sync";
+const CREATE_SYNC = "create";
+const MULTIPLE_SYNC = "multiple";
+
+const OBJECT_TYPE_ELEMENT = "element";
+const OBJECT_TYPE_RELATION = "relation";
 
 // const ENDPOINT_LABELS = {
 // 	"source.id" 	: ["source"]["id"],
@@ -18,36 +34,14 @@ const ENDPOINT_ATTRIBUTE_LABELS = ["name", "id"];
 // 	"target.name"	: ["target"]["name"]
 // }
 
-// var header = [];
-// var data = [];
-
-/**
- * uniquePropLabels
- * 	reduce function
- *  accumulate the new property labels of the given concepts
- * 
- * @param concept 
- */
-function uniquePropLabels(a, concept) {
-	concept.prop().forEach(function (propLabel) {
-		// accumulate all unique property labels. 
-		if (typeof a[propLabel] == 'undefined' ) {
-			a[propLabel] = propLabel
-			debug(`>>> accumulate a[${propLabel}]: ${a[propLabel]}`);
-		}
-	})
-	return a
-}
-
 /**
  * map a header for all the exported columns of an element
  * 
  * @param concepts
  */
-function mapHeader(concepts)
-{	
-	// accumulate all unique labels to one object
-	const propertyLabelsObject	= concepts.reduce((a, concept) => uniquePropLabels(a, concept),{})
+function mapHeader(concepts) {
+	// deduplicate the property labels  ### kan dit met een filter op een array?
+	const propertyLabelsObject = concepts.reduce((a, concept) => deduplicatePropLabels(a, concept), {})
 	// convert object with labels to array with labels
 	const propertyLabels = Object.keys(propertyLabelsObject)
 
@@ -64,15 +58,14 @@ function mapHeader(concepts)
  * 
  * @param concepts
  */
-function mapRelationHeader(concepts)
-{	
+function mapRelationHeader(concepts) {
 	let header = mapHeader(concepts)
 
 	// relation endpoint (source en target) attributes labels
 	var endpointLabels = []
-	ENDPOINT_ATTRIBUTE_LABELS.forEach(function (attribute) {
-		ENDPOINT_LABELS.forEach(function (endpoint) {
-			endpointLabels.push(`${endpoint}.${attribute}`)
+	ENDPOINT_LABELS.forEach(function (label) {
+		ENDPOINTS.forEach(function (endpoint) {
+			endpointLabels.push(`${endpoint}.${label}`)
 		})
 	})
 
@@ -85,116 +78,214 @@ function mapRelationHeader(concepts)
 }
 
 /**
- * Export an element as a csv row
+ * uniquePropLabels
+ * 	reduce function
+ *  accumulate the new property labels of the given concepts
  * 
  * @param concept 
  */
-function mapData(concept) {
-	let row = new Object;
-
-	ATTRIBUTE_LABELS.forEach(function (attribute) {
-		row[attribute] = concept[attribute];
-	})
-
-	debug(`>> ${concept}`)
-	concept.prop().forEach(function (propertyLabel) {
-		if (concept.prop(propertyLabel)) {
-			row[propertyLabel] = concept.prop(propertyLabel);
-			debug(`>> Row[${propertyLabel}]: ${row[propertyLabel]}`);
+function deduplicatePropLabels(a, concept) {
+	concept.prop().forEach(function (propLabel) {
+		// accumulate all unique property labels. 
+		if (typeof a[propLabel] == 'undefined') {
+			a[propLabel] = propLabel
+			debug(`>>> accumulate a[${propLabel}]: ${a[propLabel]}`);
 		}
 	})
-
-	debug(`>> Row ${JSON.stringify(row)}`)
-	return row
+	return a
 }
 
 /**
- * Export relation of given concepts
+ * Export an object (element of relation) as a csv row
  * 
  * @param concept 
  */
-function mapRelationData(concept) {
-	_commonShowDebugMessage.push(false);
+function mapData(concept, object_type) {
+	let row = new Object;
 
-	let row = mapData(concept)
+	_commonShowDebugMessage.push(true);
+	debug(`> ${concept}`)
 
-	// export relation endpoint (source en target) attributes
-	ENDPOINT_LABELS.forEach(function (endpoint) {
-		ATTRIBUTE_LABELS.forEach(function (attribute) {
-			row[`${endpoint}.${attribute}`] = concept[endpoint][attribute];
-			debug(`>>>> Row[${endpoint}.${attribute}]: ${concept[endpoint][attribute]}`);
-		})
+	// export attributes
+	ATTRIBUTE_LABELS.forEach(function (attribute) {
+		row[attribute] = concept[attribute];
+		debug(`>> Attr[${attribute}]: ${row[attribute]}`);
 	})
 
-	debug(`>> Row ${JSON.stringify(row)}`)
+	// export properties
+	concept.prop().forEach(function (propertyLabel) {
+		if (concept.prop(propertyLabel)) {
+			row[propertyLabel] = concept.prop(propertyLabel);
+			debug(`>> Prop[${propertyLabel}]: ${row[propertyLabel]}`);
+		}
+	})
+
+	if (object_type == OBJECT_TYPE_RELATION) {
+
+		// export relation endpoint (source en target) attributes
+		ENDPOINTS.forEach(function (endpoint) {
+			ENDPOINT_LABELS.forEach(function (label) {
+				if (label != ENDPOINT_PROP_ID) {
+					row[`${endpoint}.${label}`] = concept[endpoint][label];
+					debug(`>> Endpoint[${endpoint}.${label}]: ${concept[endpoint][label]}`);
+				} else {
+					row[`${endpoint}.${label}`] = concept[endpoint].prop(PROP_ID);
+					debug(`>> Endpoint[${endpoint}.${label}]: ${concept[endpoint].prop(PROP_ID)}`);
+				}
+			})
+		})
+	}
+
+	debug(`> Row ${JSON.stringify(row)}`)
 
 	_commonShowDebugMessage.pop();
 	return row
 }
 
 /** 
- * findObject
- * 	find object with PROP_ID, archi id or name, in that order.
+ * findArchiObject
+ * 	find object (element, relation or view) with PROP_ID, archi id or name respectively.
  */
-function findObject(row, index) {
-	let foundObjects={};
+function findArchiObject(row, index) {
+	let foundObjects = {};
+
+	rowNr = index + 2 // index plus 2 (header en start bij 0)
 
 	// search with property PROP_ID
 	if (row[PROP_ID]) {
 		foundObjects = $(row['type']).filter(c => c.prop(PROP_ID) == row[PROP_ID])
-		if (foundObjects.size()==1) {
-			return {modelObject: foundObjects.first(), row:row, index:index, action:PROP_ID_SYNC}
+		if (foundObjects.size() == 1) {
+			return {
+				modelObject: foundObjects.first(),
+				row: row,
+				index: rowNr,
+				action: PROP_ID_SYNC
+			}
 		}
 	}
 	// search for equal archi id
 	if (row['id']) {
 		foundObjects = $(`#${row['id']}`)
-		if (foundObjects.size()==1) {
-			return {modelObject: foundObjects.first(), row:row, index:index, action:ID_SYNC}
-		}
-	} 
-
-	if (row['type'].endsWith("relationship")) {
-		// ####
-		console.log(`> row is a ${row['type']}, not supported yet`)
-		// var relationships = $(source).outRels(type).filter(function(r) 
-		// 	{return r.target.equals(target)}).filter(function(r) {return r.prop(syncPropName)}).first();
-		// voor relaties zoeken op source en target
-	} else {
-		// search by name. for  an object of the same type by name (and solve if more than one object with name exists)
-		if (row['name']) {
-			foundObjects = $(`.${row['name']}`).filter(row['type'])
-			if (foundObjects.size()==1) {
-				return {modelObject: foundObjects.first(), row:row, index:index, action:ID_SYNC}
+		if (foundObjects.size() == 1) {
+			return {
+				modelObject: foundObjects.first(),
+				row: row,
+				index: rowNr,
+				action: ID_SYNC
 			}
 		}
 	}
 
-	if (foundObjects.size()==0) {
-		console.log(`> No objects found for row[${index}] (name=${row['name']}; id=${row['id']}; ${PROP_ID}=${row[PROP_ID]})`)
-		return {modelObject: foundObjects.first(), row:row, index:index, action:CREATE_SYNC}
+	// search by name only for elements, not for relations
+	if (!row['type'].endsWith("relationship")) {
+		if (row['name'] && row['type']) {
+			foundObjects = $(`.${row['name']}`).filter(row['type'])
+			if (foundObjects.size() == 1) {
+				return {
+					modelObject: foundObjects.first(),
+					row: row,
+					index: rowNr,
+					action: NAME_SYNC
+				}
+			}
+		}
 	} else {
-		console.log(`> Multiple objects found for row[${index}] (name=${row['name']}; id=${row['id']}; ${PROP_ID}=${row[PROP_ID]})`)
-		foundObjects.forEach(o => console.log(`>> ${o}`))
-		return {modelObject: foundObjects.first(), row:row, index:index, action:MULTIPLE_SYNC}
+		// search relation by endpoints (source and targets)
+		console.log(`> row is a ${row['type']}, not supported yet`)
+
+		source = findElement(row[`source.prop(${PROP_ID})`], row[`source.id`])
+		target = findElement(row[`target.prop(${PROP_ID})`], row[`target.id`])
+
+		if (source && target) {
+
+			foundObjects = $(source).outRels(row['type']).filter(function (r) {
+				return r.target.equals(target)
+			});
+			if (foundObjects.size() == 1) {
+				return {
+					modelObject: foundObjects.first(),
+					row: row,
+					index: rowNr,
+					action: ENDPOINT_SYNC
+				}
+			}
+		}
 	}
+
+	if (foundObjects.size() == 0) {
+		console.log(`> No objects found for row[${rowNr}] (name=${row['name']}; id=${row['id']}; ${PROP_ID}=${row[PROP_ID]})`)
+		return {
+			modelObject: foundObjects.first(),
+			row: row,
+			index: rowNr,
+			action: CREATE_SYNC
+		}
+	} else {
+		console.log(`> Multiple objects found for row[${rowNr}] (name=${row['name']}; id=${row['id']}; ${PROP_ID}=${row[PROP_ID]})`)
+		foundObjects.forEach(o => console.log(`>> ${o}`))
+		return {
+			modelObject: foundObjects.first(),
+			row: row,
+			index: rowNr,
+			action: MULTIPLE_SYNC
+		}
+	}
+}
+
+/** 
+ */
+function findElement(e_prop_id, e_id) {
+	let foundObjects = {};
+
+	// search with property PROP_ID
+	if (e_prop_id) {
+		foundObjects = $(row['type']).filter(c => c.prop(PROP_ID) == e_prop_id)
+		if (foundObjects.size() == 1) {
+			return foundObjects.first()
+		}
+	}
+	// search for equal archi id
+	if (e_id) {
+		foundObjects = $(`#${e_id}`)
+		if (foundObjects.size() == 1) {
+			return foundObjects.first()
+		}
+	}
+}
+
+/** 
+ * createObject
+ * 	create an object for the row
+ */
+function createObject(row) {
+	let line = '';
+
+	let object = model.createElement(row.type, row.name);
+	line = `> createObject: ${object}\n`
+
+	line += syncObject(object, row)
+	return line
 }
 
 /** 
  * syncObject
  * 	synchronize the model object with row values
  */
-function syncObject(object, row) {
+function syncObject(scan) {
 	let lineUpdated = '';
 	let line = '';
-	
-	// convert object to key's array
+
+	object = scan.modelObject
+	row = scan.row
+
+	// convert object key's to an array
 	const rowLabels = Object.keys(row);
-	// filter the attribute id, the id can't be changed
-	const importlabels  = rowLabels.filter(label => (SKIP_ATTRIBUTE_ID.indexOf(label) == -1 ));
-	
+	// don't import the attribute id, as it can't be changed
+	const importlabels = rowLabels.filter(label => (SKIP_IMPORT_LABELS.indexOf(label) == -1));
+
+	// update objects attributes and properties with the row cell values
 	importlabels.map(label => {
-		// if row has a value and row value is different than object value
+		// skip row cell if empty or if equal to object value
 		if ((row[label]) && (row[label] != get_attr_or_prop(object, label))) {
 			if (get_attr_or_prop(object, label)) {
 				lineUpdated += `\n>> Update [${label}]: \n${get_attr_or_prop(object, label)}\n${row[label]}`;
@@ -204,12 +295,12 @@ function syncObject(object, row) {
 			set_attr_or_prop(object, row, label)
 		}
 	})
-	
+
 	if (lineUpdated) {
-		line = `> Update: ${object}`
+		line = `> Update from row[${scan.index}]: ${object}`
 		line += lineUpdated
 	}
-	
+
 	// const endpointLabels = rowLabels.filter(label => label in ENDPOINT_LABELS);
 	// line += endpointLabels.map(label => {
 	// 	let line=''
@@ -226,7 +317,7 @@ function syncObject(object, row) {
 }
 
 function get_attr_or_prop(object, label) {
-	if (ATTRIBUTE_LABELS.indexOf(label) != -1 ) {
+	if (ATTRIBUTE_LABELS.indexOf(label) != -1) {
 		return object[label]
 	} else {
 		return object.prop(label)
@@ -234,25 +325,11 @@ function get_attr_or_prop(object, label) {
 }
 
 function set_attr_or_prop(object, row, label) {
-	if (ATTRIBUTE_LABELS.indexOf(label) != -1 ) {
+	if (ATTRIBUTE_LABELS.indexOf(label) != -1) {
 		object[label] = row[label]
 	} else {
 		object.prop(label, row[label])
 	}
-}
-
-/** 
- * createObject
- * 	create an object for the row
- */
-function createObject(row) {
-	let line = '';
-
-	let object = model.createElement(row.type, row.name);
-	line = `> createObject: ${object}\n`
-
-	line += syncObject(object, row)
-	return line
 }
 
 /**
@@ -278,8 +355,10 @@ function save2file(header, data, dataName) {
 	});
 
 	if (exportFile != null) {
-		$.fs.writeFile(exportFile, Papa.unparse({ fields: header, data: data }));
-		console.log(`Exported ${data.length} rows`)
+		$.fs.writeFile(exportFile, Papa.unparse({
+			fields: header,
+			data: data
+		}));
 		console.log("Saved to file : " + exportFile);
 	} else {
 		console.log("\nExport CSV canceled");
@@ -293,7 +372,11 @@ function loadData() {
 
 	if (filePath) {
 		console.log(`> Loading ${filePath} from CSV...`);
-		var rows = Papa.parse(readFully(filePath, 'utf-8'), { header: true, encoding: 'utf-8', skipEmptyLines: true }).data;
+		var rows = Papa.parse(readFully(filePath, 'utf-8'), {
+			header: true,
+			encoding: 'utf-8',
+			skipEmptyLines: true
+		}).data;
 
 	} else {
 		console.log("> Canceled");
@@ -303,30 +386,30 @@ function loadData() {
 
 // Some Polyfills for Nashorn ====================================================================================
 function readFully(url, charset) {
-    var result = '';
-    var imports = new JavaImporter(java.net, java.lang, java.io);
+	var result = '';
+	var imports = new JavaImporter(java.net, java.lang, java.io);
 
-    with (imports) {
+	with(imports) {
 
-        var urlObj = null;
+		var urlObj = null;
 
-        try {
-            urlObj = new URL(url);
-        } catch (e) {
-            // If the URL cannot be built, assume it is a file path.
-            urlObj = new URL(new File(url).toURI().toURL());
-        }
+		try {
+			urlObj = new URL(url);
+		} catch (e) {
+			// If the URL cannot be built, assume it is a file path.
+			urlObj = new URL(new File(url).toURI().toURL());
+		}
 
-        var reader = new BufferedReader(new InputStreamReader(urlObj.openStream(), charset));
+		var reader = new BufferedReader(new InputStreamReader(urlObj.openStream(), charset));
 
-        var line = reader.readLine();
-        while (line != null) {
-            result += line + '\n';
-            line = reader.readLine();
-        }
+		var line = reader.readLine();
+		while (line != null) {
+			result += line + '\n';
+			line = reader.readLine();
+		}
 
-        reader.close();
-    }
+		reader.close();
+	}
 
-    return result;
+	return result;
 }
