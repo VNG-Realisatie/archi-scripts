4f093ec075b43a48f14ee5f533c6f4d5941cb589
Mark Backer <mark.backer@vng.nl>
2021-10-31 19:02:39 +0100

    Table with ascii-table; remove copy text-table

--- _lib/Common.js
+++ _lib/Common.js
@@ -16,8 +16,8 @@ let _logDebugMessage = [false];
 
 const JS_NASHORN_ES6 = "jdk.nashorn.api.scripting.NashornScriptEngine";
 const JS_ENGINE_GRAALVM = "com.oracle.truffle.js.scriptengine.GraalJSScriptEngine";
-const JS_ENGINES = [ JS_NASHORN_ES6, JS_ENGINE_GRAALVM];
-const JS_ENGINES_TEXT = [ "Nashorn ES6", "GraalVM"];
+const JS_ENGINES = [JS_NASHORN_ES6, JS_ENGINE_GRAALVM];
+const JS_ENGINES_TEXT = ["Nashorn ES6", "GraalVM"];
 
 /**
  * initConsoleLog and finishconsole
@@ -90,7 +90,9 @@ function check_JS_Engine(required_engine) {
       line += "\n- Required engine: " + JS_ENGINES_TEXT[required_js_index] + "\n";
       line += "\nChange your Archi settings";
       line +=
-        "\n- Go to Edit > Preferences > Scripting and select the '" + JS_ENGINES_TEXT[required_js_index] + "' JavaScript Engine";
+        "\n- Go to Edit > Preferences > Scripting and select the '" +
+        JS_ENGINES_TEXT[required_js_index] +
+        "' JavaScript Engine";
       line += "\n- If the JavaScript engine is not selectable, first upgrade the jArchi plugin\n\n";
       console.error(line);
     } else {
@@ -202,163 +204,4 @@ function slash() {
       throw `>> Unknown OS: ${nameOS}`;
     }
   }
-}
-
-/**
- * function table
- *   generate text table strings suitable for printing to stdout
- *   usage: var s = table(rows, opts={})
- *
- * based on https://www.npmjs.com/package/text-table
- *
- * options are
- *   opts.markdown - generate a markdown table
- *   opts.hsep     - separator to use between columns, default '  '
- *   opts.align    - array of alignment types for each column, default ['l','l',...]
- *   opts.stringLength - callback function to use when calculating the string length
- *
- * alignment types are:
- *   'l' - left
- *   'r' - right
- *   'c' - center
- *   '.' - decimal
- *
- * @param {*} tData 2 dimensional array with rows and columns
- * @param {*} opts object with layout options
- * @returns string with table
- */
-function table(tData, opts) {
-  const cLineChar = "-";
-  const cIndexTitle = 1;
-  const cIndexLine = 2;
-
-  if (!opts) opts = {};
-  var markDown = opts.markdown === true ? true : false;
-  if (markDown) {
-    var hsep = " | ";
-  } else {
-    var hsep = opts.hsep === undefined ? "  " : opts.hsep;
-  }
-  var align = opts.align || [];
-  var stringLength =
-    opts.stringLength ||
-    function (s) {
-      return String(s).length;
-    };
-
-  // if option markdown is set, create and insert a row with the markdown title separator
-  var rows_ = [];
-  if (markDown) {
-    var i_rowsTitle = 0;
-    for (let i_row = 0; i_row < tData.length; i_row++) {
-      rows_[i_rowsTitle] = tData[i_row];
-      i_rowsTitle++;
-      if (i_row == 0) {
-        rows_[i_rowsTitle] = [];
-        for (let i_cell = 0; i_cell < tData[i_row].length; i_cell++) {
-          rows_[i_rowsTitle][i_cell] = cLineChar;
-        }
-        i_rowsTitle++;
-      }
-    }
-  } else {
-    rows_ = tData;
-  }
-
-  var dotsizes = reduce(
-    rows_,
-    function (acc, row) {
-      forEach(row, function (c, ix) {
-        let n = dotindex(c);
-        if (!acc[ix] || n > acc[ix]) acc[ix] = n;
-      });
-      return acc;
-    },
-    []
-  );
-
-  // for collumns with '.', find the dot and fill out to the right
-  var irow = 0;
-  var rows = map(rows_, function (row) {
-    irow++;
-    return map(row, function (c_, ix) {
-      let c = String(c_);
-      if (
-        align[ix] === "." &&
-        (!markDown || // there is no title
-          (markDown && irow != cIndexTitle && irow != cIndexLine)) // skip the Title rows
-      ) {
-        let index = dotindex(c);
-        let size = dotsizes[ix] + (/\./.test(c) ? -1 : 0) - (stringLength(c) - index);
-
-        return c + Array(size).join(" ");
-      } else return c;
-    });
-  });
-
-  var sizes = reduce(
-    rows,
-    function (acc, row) {
-      forEach(row, function (c, ix) {
-        let n = stringLength(c);
-        if (!acc[ix] || n > acc[ix]) acc[ix] = n;
-      });
-      return acc;
-    },
-    []
-  );
-
-  var irow = 0;
-  var tableString = map(rows, function (row) {
-    irow++;
-    return (
-      `${markDown ? "| " : ""}` +
-      map(row, function (c, ix) {
-        let n = sizes[ix] - stringLength(c) || 0;
-        let s = Array(Math.max(n + 1, 1)).join(" ");
-        if (markDown && irow == cIndexLine) {
-          return c + Array(Math.max(n + 1, 1)).join(cLineChar);
-        }
-        if (align[ix] === "c" || (markDown && irow == cIndexTitle)) {
-          return Array(Math.ceil(n / 2 + 1)).join(" ") + c + Array(Math.floor(n / 2 + 1)).join(" ");
-        }
-        if (align[ix] === "r" || align[ix] === ".") {
-          return s + c;
-        }
-        return c + s;
-      }).join(hsep)
-    ); //.replace(/\s+$/, '');
-  }).join(`${markDown ? " |" : ""}\n`);
-  return tableString + `${markDown ? " |" : ""}\n`;
-}
-
-function dotindex(c) {
-  let m = /\.[^.]*$/.exec(c);
-  return m ? m.index + 1 : c.length;
-}
-
-function reduce(xs, f, init) {
-  if (xs.reduce) return xs.reduce(f, init);
-  let i = 0;
-  let acc = arguments.length >= 3 ? init : xs[i++];
-  for (; i < xs.length; i++) {
-    f(acc, xs[i], i);
-  }
-  return acc;
-}
-
-function forEach(xs, f) {
-  if (xs.forEach) return xs.forEach(f);
-  for (let i = 0; i < xs.length; i++) {
-    f.call(xs, xs[i], i);
-  }
-}
-
-function map(xs, f) {
-  if (xs.map) return xs.map(f);
-  let res = [];
-  for (let i = 0; i < xs.length; i++) {
-    res.push(f.call(xs, xs[i], i));
-  }
-  return res;
 }
